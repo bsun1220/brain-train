1a00b40afb37b9687e97bc76ef12f43ce52bb3c0
bsun1220 <65567539+bsun1220@users.noreply.github.com>
2022-05-31 20:01:45 -0400

    added reaction game

--- client/src/components/click_game.js
+++ client/src/components/click_game.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useInsertionEffect } from "react";
+import React, { useState, useEffect, useReducer} from "react";
 import styles from "./global.module.css";
 import Green_Page from "./green-page";
 import Red_Page from "./red-page"
@@ -8,18 +8,95 @@ import End_Page from "./end-page";
 
 export default function Click(){
 
-    return(
-        <button className = {styles.reaction_game}>
-            <Green_Page/>
-            <Red_Page/>
-            <Start_Page/>
-            <Blue_Page correct = {false} time = {1232}/>
-        </button>
-       
-       
-       /*<div className = {styles.reaction_game}>
-        <End_Page avg = {123}/>
-       </div>*/
-    )
+    const [outcome, setOutcome] = useState({
+        "page":"start",
+        is_red: false,
+        "greenStart":0
+    })
+    
+    const [scores, setScore] = useState([]);
+    const [round, addRound] = useReducer(
+        (number) => number + 1,
+        1
+    );
+
+    const ref = {
+        "start":<Start_Page />,
+        "red":<Red_Page/>,
+        "blue":<Blue_Page/>,
+        "score":<Blue_Page correct = {true} time = {scores.slice(-1)[0]}/>,
+        "green":<Green_Page/>
+    };
+
+    const random_timer = () => Math.random() * 5000 + 2000;
+
+    let current = outcome["page"];
+
+    useEffect(() => {
+        if(outcome["page"] == "red"){
+            const timer = setTimeout(() =>{
+                if(current == "red"){
+                    setOutcome((val) =>{
+                        return {...val,...{"page":"green", is_red:false, greenStart:Date.now()}};
+                    });
+                    current = "green";
+                }
+            }, random_timer());
+            return () => clearTimeout(timer);
+        }
+      }, [outcome["is_red"]]);
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        if(outcome["page"] == "start"){
+            setOutcome((val) => {
+                return {...val,... {"page":"red", is_red:true}}
+            });
+            current = "red";
+        }
+        if(outcome["page"] == "red"){
+            setOutcome((val) => {
+                return {...val,... {"page":"blue", is_red:false}}
+            });
+            current = "blue";
+        }
+        if(outcome["page"] == "blue"){
+            setOutcome((val) => {
+                return {...val,... {"page":"red", is_red:true}}
+            });
+            current = "red";
+        }
+        if(outcome["page"] == "green"){
+        
+            setOutcome((val) => {
+                return {...val,... {"page":"score", is_red:false}}
+            });
+            setScore((oldArray) => [...oldArray, Date.now() - outcome["greenStart"]]);
+            current = "blue";
+        }
+
+        if (outcome["page"] == "score"){
+            setOutcome((val) => {
+                return {...val,... {"page":"red", is_red:true}}
+            });
+            current = "red";
+            addRound();
+        }
+    }
+
+
+    if (round <= 5){
+        return(
+            <button className = {styles.reaction_game} onClick = {handleClick}>
+                {ref[outcome["page"]]}
+            </button>);
+    }
+    else{
+        return(
+            <div className = {styles.reaction_game}>
+                <End_Page data = {scores}/>
+            </div>
+        )
+    }
 
 }
--- client/src/components/end-page.js
+++ client/src/components/end-page.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect, useInsertionEffect } from "react";
+import React from "react";
 import styles from "./global.module.css";
 
 export default function End_Page(props){
+
+    const get_avg = () => Math.floor(props.data.reduce((a,b) => a + b, 0)/props.data.length);
 
     return(
         <div className = {styles.click_page} id = {styles.blue_page}> 
@@ -10,7 +12,7 @@ export default function End_Page(props){
                 <div id = {styles.center}>
                     <img id = {styles.smiley} src = "smiley.png" width = "100px"/>
                 </div>
-                <h1> Your average was {props.avg} ms!</h1>
+                <h1> Your average was {get_avg()} ms!</h1>
             <div id = {styles.play_buttons}>
                 <button className = {styles.end_button}> Save Data </button>
                 <button className = {styles.end_button}> Play Again</button>
