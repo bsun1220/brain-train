94cd4c0fadfca5be35f011abad0082751c609bda
bsun1220 <65567539+bsun1220@users.noreply.github.com>
2022-06-14 20:21:08 -0400

    adjust

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.use(require("./routes/record"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.get('/',(req, res) => {
+  res.send("hi");
+});
+
 app.use(require("./routes/user"));
 app.use(require("./routes/game"));
 app.use(require("./routes/comment"));
